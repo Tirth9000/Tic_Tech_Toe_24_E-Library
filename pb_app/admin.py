37cf21ac6cb69939a89c79c6279bdb998506dc6b
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2024-09-29 15:44:21 +0530

    Delete pb_app directory
    delete

--- pb_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from pb_app.models import *
-
-# Register your models here.
-admin.site.register(LibraryUser)
-admin.site.register(Books)
-admin.site.register(FavouriteCollections)
-admin.site.register(Rating)
